9ab33fd2961dd8d6def1af72910a200e24ba33b6
habby <a0979580915@gmail.com>
2024-12-01 11:11:24 +0800

    users can set latency_budget, and subscriber can receive it

---
+++ examples/examples/latency_pub.rs
@@ -0,0 +1,72 @@
+//
+// Copyright (c) 2023 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+use std::time::Duration;
+
+use clap::Parser;
+use zenoh::{bytes::Encoding, key_expr::KeyExpr, Config};
+use zenoh_examples::CommonArgs;
+
+#[tokio::main]
+async fn main() {
+    // Initiate logging
+    zenoh::init_log_from_env_or("error");
+
+    let (config, key_expr, payload, attachment, latency_budget) = parse_args();
+
+    println!("Opening session...");
+    let session = zenoh::open(config).await.unwrap();
+
+    println!("Declaring Publisher on '{key_expr}'...");
+    let publisher = session.declare_publisher(&key_expr).await.unwrap();
+
+    println!("Press CTRL-C to quit...");
+    for idx in 0..u32::MAX {
+        tokio::time::sleep(Duration::from_secs(1)).await;
+        let buf = format!("[{idx:4}] {payload}");
+
+        let latency_budget = format!("{}", latency_budget.as_millis());
+
+        println!("Putting Data ('{}': '{}')...", &key_expr, buf);
+        // Refer to z_bytes.rs to see how to serialize different types of message
+        publisher
+            .put(buf)
+            .encoding(Encoding::TEXT_PLAIN) // Optionally set the encoding metadata 
+            .attachment(latency_budget) // Optionally add an attachment
+            .await
+            .unwrap();
+    }
+}
+
+#[derive(clap::Parser, Clone, PartialEq, Eq, Hash, Debug)]
+struct Args {
+    #[arg(short, long, default_value = "demo/example/zenoh-rs-pub")]
+    /// The key expression to write to.
+    key: KeyExpr<'static>,
+    #[arg(short, long, default_value = "Pub from Rust!")]
+    /// The payload to write.
+    payload: String,
+    #[arg(short, long)]
+    /// The attachments to add to each put.
+    attach: Option<String>,
+    #[arg(short, long, default_value_t = 0)]
+    latency_budget: u64,
+    #[command(flatten)]
+    common: CommonArgs,
+}
+
+fn parse_args() -> (Config, KeyExpr<'static>, String, Option<String>, Duration) {
+    let args = Args::parse();
+    let latency_budget = Duration::from_millis(args.latency_budget);
+    (args.common.into(), args.key, args.payload, args.attach, latency_budget)
+}
