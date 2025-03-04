df0ed70307477dc1c5d7b7559d682852089d7f5e
habby <a0979580915@gmail.com>
2025-03-04 14:44:50 +0800

    Create script for measuring latency. Users can also set date priority

---
+++ examples/examples/priority_ping.rs
@@ -0,0 +1,143 @@
+//
+// Copyright (c) 2023 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use clap::Parser;
+use zenoh::{bytes::ZBytes, qos::CongestionControl, qos::Priority, Config, Wait};
+use zenoh_examples::CommonArgs;
+
+fn main() {
+    // initiate logging
+    zenoh::init_log_from_env_or("error");
+
+    let (config, size, n, express, priority) = parse_args();
+    let session = zenoh::open(config).wait().unwrap();
+
+    let topic = "data";
+    let pong_topic = format!("{}pong", topic);
+    let key_expr_ping = topic;
+    let key_expr_pong = pong_topic;
+
+    let sub = session.declare_subscriber(&key_expr_pong).wait().unwrap();
+    let publisher = session
+        .declare_publisher(key_expr_ping)
+        .congestion_control(CongestionControl::Block)
+        .priority(priority)
+        .express(express)
+        .wait()
+        .unwrap();
+
+    // Create dummy data of user-defined size
+    let dummy_data: Vec<u8> = (0..size - 16)
+        .map(|i| (i % 256) as u8)
+        .collect();
+
+    let mut samples = Vec::with_capacity(n);
+
+    for i in 0..n {
+        let sent_timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_micros()
+            .to_be_bytes();
+        let mut data = sent_timestamp.to_vec();
+        data.extend_from_slice(&dummy_data);
+        publisher.put(ZBytes::from(data)).wait().unwrap();
+        
+        if let Ok(sample) = sub.recv() {
+            let received_payload = sample.payload();
+            let payload_bytes: std::borrow::Cow<'_, [u8]> = received_payload.to_bytes();
+                
+            let original_timestamp_bytes: [u8; 16] = payload_bytes[0..16]
+                .try_into()
+                .expect("Invalid timestamp length");
+            let original_timestamp = u128::from_be_bytes(original_timestamp_bytes);
+                
+            let current_timestamp = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_micros();
+            let time_difference = current_timestamp - original_timestamp;
+                
+            println!(
+                "Message {}: Received message with original timestamp: {:?}, Time difference: {:?}µs",
+                    i + 1,
+                    original_timestamp,
+                    time_difference,
+            );
+            
+            samples.push(time_difference);
+        }
+        else{
+            println!("Failed to receive response for message");
+        }
+    }
+
+    for (i, rtt) in samples.iter().enumerate().take(n) {
+        println!(
+            "{} bytes: seq={} rtt={:?}µs lat={:?}µs",
+            size,
+            i,
+            rtt,
+            rtt / 2
+        );
+    }
+} 
+
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short = 'n', long, default_value = "100")]
+    /// The number of round-trips to measure
+    samples: usize,
+    #[arg(short = 's', long, default_value = "128")]
+    /// Sets the size of the payload to publish
+    payload_size: usize,
+    #[arg(long, default_value = "false")]
+    /// Enable express mode
+    express: bool,
+
+    #[arg(short = 'p', long, default_value = "5")]
+    /// Set the priority level (1=RealTime, 2=InteractiveHigh, 3=InteractiveLow, 4=DataHigh, 5=Data, 6=DataLow, 7=Background)
+    priority: u8,
+
+    #[command(flatten)]
+    common: CommonArgs,
+}
+
+fn parse_args() -> (Config, usize, usize, bool, Priority) {
+    let args = Args::parse();
+
+    let priority = match args.priority {
+        1 => Priority::RealTime,
+        2 => Priority::InteractiveHigh,
+        3 => Priority::InteractiveLow,
+        4 => Priority::DataHigh,
+        5 => Priority::Data,
+        6 => Priority::DataLow,
+        7 => Priority::Background,
+        _ => {
+            eprintln!("Invalid priority value. Using default (Data = 5).");
+            Priority::Data
+        }
+    };
+
+    (
+        args.common.into(),
+        args.payload_size,
+        args.samples,
+        args.express,
+        priority,
+    )
+}
---
+++ examples/examples/priority_pong.rs
@@ -0,0 +1,111 @@
+//
+// Copyright (c) 2023 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+use clap::Parser;
+use zenoh::{
+    key_expr::keyexpr,
+    qos::{CongestionControl, Priority},
+    Config, Wait,
+};
+use zenoh_examples::CommonArgs;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn main() {
+    // Initialize logging
+    zenoh::init_log_from_env_or("error");
+
+    let (config, express, priority) = parse_args();
+    let session = zenoh::open(config).wait().unwrap();
+
+    let topic = "data";
+    let pong_topic = format!("{}pong", topic);
+    let pong_topic_static: &'static str = Box::leak(pong_topic.into_boxed_str());
+    let key_expr_ping = keyexpr::new(topic).unwrap();
+    let key_expr_pong = keyexpr::new(pong_topic_static).unwrap();
+
+    // Declare publisher for pong messages
+    let publisher = session
+        .declare_publisher(key_expr_pong)
+        .congestion_control(CongestionControl::Block)
+        .priority(priority)
+        .express(express)
+        .wait()
+        .unwrap();
+
+    // Declare subscriber for ping messages and respond with pong
+    session
+        .declare_subscriber(key_expr_ping)
+        .callback(move |sample| {
+            let received_payload = sample.payload();
+            let payload_bytes: std::borrow::Cow<'_, [u8]> = received_payload.to_bytes();
+
+            let original_timestamp_bytes: [u8; 16] = payload_bytes[0..16]
+                .try_into()
+                .expect("Invalid timestamp length");
+            let original_timestamp = u128::from_be_bytes(original_timestamp_bytes);
+
+            let current_timestamp = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_micros();
+            let time_difference = current_timestamp - original_timestamp;
+
+            println!(
+                "Received message with original timestamp: {:?}µs, Time difference: {:?}µs",
+                original_timestamp, time_difference,
+            );
+
+            publisher.put(received_payload.clone()).wait().unwrap();
+        })
+        .background()
+        .wait()
+        .unwrap();
+
+    // Park the thread to keep the process running
+    std::thread::park();
+}
+
+#[derive(clap::Parser, Clone, PartialEq, Eq, Hash, Debug)]
+struct Args {
+    /// express for sending data
+    #[arg(long, default_value = "false")]
+    no_express: bool,
+
+    /// Set priority level (1=RealTime, 2=InteractiveHigh, 3=InteractiveLow, 4=DataHigh, 5=Data, 6=DataLow, 7=Background)
+    #[arg(short = 'p', long, default_value = "5")]
+    priority: u8,
+
+    #[command(flatten)]
+    common: CommonArgs,
+}
+
+fn parse_args() -> (Config, bool, Priority) {
+    let args = Args::parse();
+
+    let priority = match args.priority {
+        1 => Priority::RealTime,
+        2 => Priority::InteractiveHigh,
+        3 => Priority::InteractiveLow,
+        4 => Priority::DataHigh,
+        5 => Priority::Data,
+        6 => Priority::DataLow,
+        7 => Priority::Background,
+        _ => {
+            eprintln!("Invalid priority value. Using default (Data = 5).");
+            Priority::Data
+        }
+    };
+
+    (args.common.into(), !args.no_express, priority)
+}
+
